b11de05f5dfcd4de5adb9a1d0b6e9debf9431c4f
dopeldead <gidon@intechinfo.fr>
2017-01-15 17:38:23 +0100

    test shit

--- Client/src/app/components/menu/menu.ts
+++ Client/src/app/components/menu/menu.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Channel } from 'models';
-
+import { ChannelService } from '../../services/index';
+require('Promise');
 @Component({
     selector: 'menu',
     templateUrl: 'menu.html'
 })
-export class MenuComponent {
-    @Input() channels: Channel[] = [];
+export class MenuComponent { 
+   model = new Channel();
+   data = new Promise<Channel[]>((e)=>{return e;})
+    constructor( private channelService: ChannelService ) {
+        this.data = this.channelService.getAll().then((e)=> { return e;});
+    }
+     save() {
+        this.channelService.add(this.model.name)
+    }
+    @Input() channels: Channel[] = Promise.resolve<Channel[]>(this.channelService.getAll());
+   
 }
