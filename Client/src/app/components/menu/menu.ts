3966e86aae9b9888538a59dc7dd7c116875ebd9f
dopeldead <gidon@intechinfo.fr>
2017-01-27 09:54:41 +0100

    working channles, need now to do the posts

--- Client/src/app/components/menu/menu.ts
+++ Client/src/app/components/menu/menu.ts
@@ -1,20 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,OnInit, EventEmitter } from '@angular/core';
 import { Channel } from 'models';
 import { ChannelService } from '../../services/index';
-require('Promise');
+
 @Component({
     selector: 'menu',
     templateUrl: 'menu.html'
 })
-export class MenuComponent { 
-   model = new Channel();
-   data = new Promise<Channel[]>((e)=>{return e;})
-    constructor( private channelService: ChannelService ) {
-        this.data = this.channelService.getAll().then((e)=> { return e;});
+export class MenuComponent implements OnInit { 
+    model = new Channel();
+
+    constructor(private channelService: ChannelService) {
     }
-     save() {
-        this.channelService.add(this.model.name)
+    async ngOnInit() { 
+        this.channels = (await this.channelService.getAll()).sort(this.sortChannels);
     }
-    @Input() channels: Channel[] = Promise.resolve<Channel[]>(this.channelService.getAll());
+    async save() {
+         if(this.channels.findIndex(x=>x.name===this.model.name)===-1){
+            await this.channelService.add(this.model.name);
+            this.channels = await this.channelService.getAll();
+            this.channels.sort(this.sortChannels);
+         }
+         else{
+             //chan name already exist
+         }
+    }
+    callParent(chanName: string){
+        this.model.name=chanName;
+        this.save();
+    }
+    sortChannels(n1: Channel,n2: Channel){
+    if (n1.name > n2.name) {
+        return 1;
+    }
+    if (n1.name < n2.name) {
+        return -1;
+    }
+
+    return 0;
+}
+    @Input() channels: Channel[]; 
    
 }
