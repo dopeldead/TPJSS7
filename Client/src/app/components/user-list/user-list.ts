ef3cbd62486f8a5dbd91d4ed27891e8375c47c3d
dopeldead <gidon@intechinfo.fr>
2017-01-31 10:56:18 +0100

    wip liste

--- Client/src/app/components/user-list/user-list.ts
+++ Client/src/app/components/user-list/user-list.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { PostSocketService, LoggedUser } from 'services';
 
 @Component({
     selector: 'user-list',
     templateUrl: 'user-list.html'
 })
 export class UserListComponent implements OnInit {
-    constructor() { }
+    constructor( private postSocket: PostSocketService,
+            private user: LoggedUser
+) { }
 
     ngOnInit() { }
 }
